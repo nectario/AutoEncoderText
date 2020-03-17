cf798be152381a95d35b0a2733bd004f4ee35673
Nektarios Kalogridis <Nektarios.Kalogridis@viacomcontractor.com>
2020-03-17 18:57:55 -0400

    GenreAutoML

--- GenrePredictionAutoML.py
+++ GenrePredictionAutoML.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 
+import autokeras as ak
+from GenrePredictionModel import *
+
 def imdb_raw():
     max_features = 20000
     index_offset = 3  # word index offset
@@ -8,7 +11,6 @@ def imdb_raw():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(
         num_words=max_features,
         index_from=index_offset)
-
     x_train = x_train
     y_train = y_train.reshape(-1, 1)
     x_test = x_test
@@ -28,3 +30,37 @@ def imdb_raw():
     x_train = np.array(x_train, dtype=np.str)
     x_test = np.array(x_test, dtype=np.str)
     return (x_train, y_train), (x_test, y_test)
+
+
+def genre_prediction():
+    vectorizer = MultiVectorizer()
+    genre_prediction = GenrePredictionModel(vectorizer=vectorizer)
+
+    training_data_df, validation_data_df = genre_prediction.load_data("data/film_data_lots.xlsx", no_sentences=True)
+
+    clf = ak.TextClassifier(max_trials=4, multi_label=True)
+
+    X_train = np.array(training_data_df["Subtitles"].tolist())
+    y_train = genre_prediction.training_labels
+
+    X_validation = np.array(validation_data_df["Subtitles"].tolist())
+    y_validation = genre_prediction.validation_labels
+
+    clf.fit(X_train, y_train, validation_data=(X_validation, y_validation))
+
+
+
+genre_prediction()
+
+# # Prepare the data.
+# (x_train, y_train), (x_test, y_test) = imdb_raw()
+# print(x_train.shape)  # (25000,)
+# print(y_train.shape)  # (25000, 1)
+# print(x_train[0][:50])  # <START> this film was just brilliant casting <UNK>
+#
+# # Initialize the TextClassifier
+# clf = ak.TextClassifier(max_trials=3)
+# # Search for the best model.
+# clf.fit(x_train, y_train, epochs=2)
+# # Evaluate on the testing data.
+# print('Accuracy: {accuracy}')
